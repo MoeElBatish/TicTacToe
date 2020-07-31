142a0783efd8c242d275ab58a1f3924868ce2ee0
Moe Elbatish <moebatish@gmail.com>
2020-07-31 13:13:52 -0400

    Added gameboard module pattern

---
+++ script/app.js
@@ -0,0 +1,17 @@
+// gameBoard - module pattern game board, stores the positions of each box and has functions to check states
+const gameBoard = (function () {
+    //let boxes = document.querySelectorAll(".box")
+    let board = [[],[],[]];
+    const clearBoard = function ()  {
+        board = [[],[],[]];
+    };
+    const winCheck = function ()  {
+
+    };
+    const placeMarker = function (row, col) {
+
+    }
+    const validBox = function (row, col) {
+        
+    }
+})
